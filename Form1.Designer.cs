b7a52e48851e9a19eba6c90852fcc3f6c5447106
Nguyen Le <nguyenle871990@gmail.com>
2021-06-02 16:33:30 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace MemorySpiel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.labelTimeClicked = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MemorySpiel.Properties.Resources._00;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MemorySpiel.Properties.Resources._00;
+            this.pictureBox2.Location = new System.Drawing.Point(82, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MemorySpiel.Properties.Resources._00;
+            this.pictureBox3.Location = new System.Drawing.Point(162, 79);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::MemorySpiel.Properties.Resources._00;
+            this.pictureBox4.Location = new System.Drawing.Point(162, 156);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(74, 71);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // labelTimeClicked
+            // 
+            this.labelTimeClicked.AutoSize = true;
+            this.labelTimeClicked.Location = new System.Drawing.Point(82, 280);
+            this.labelTimeClicked.Name = "labelTimeClicked";
+            this.labelTimeClicked.Size = new System.Drawing.Size(76, 15);
+            this.labelTimeClicked.TabIndex = 4;
+            this.labelTimeClicked.Text = "Time clicked:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 362);
+            this.Controls.Add(this.labelTimeClicked);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label labelTimeClicked;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemorySpiel
+{
+    public partial class Form1 : Form
+    {
+        private int timeClicked = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flip(PictureBox pictureBox)
+        {
+            pictureBox.Image = Properties.Resources._00;
+        }
+        private void compare(PictureBox box1, PictureBox box2)
+        {
+            if (box1.Image.Tag == box2.Image.Tag)
+            {
+                MessageBox.Show("gut", "gut", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                box1.Enabled = false;
+                box2.Enabled = false;
+            }
+            else
+            {
+                flip(box1);
+                flip(box2);
+                MessageBox.Show("falsch", "flasch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void click(PictureBox pictureBox, String tag)
+        {
+            if ((timeClicked % 2) == 0)
+            {
+                pictureBox1.Image = Properties.Resources._02;
+                pictureBox1.Image.Tag = tag;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources._00;
+                pictureBox1.Image.Tag = "00";
+            }
+            timeClicked++;
+            labelTimeClicked.Text = "Time Clicked: " + timeClicked.ToString();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if ((timeClicked % 2) == 0)
+            {
+                pictureBox1.Image = Properties.Resources._01;
+                pictureBox1.Image.Tag = "01";
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources._00;
+                pictureBox1.Image.Tag = "00";
+            }
+            timeClicked++;
+            labelTimeClicked.Text = "Time Clicked: " + timeClicked.ToString();
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if ((timeClicked % 2) == 0)
+            {
+                pictureBox4.Image = Properties.Resources._02;
+                pictureBox4.Image.Tag = "02";
+            }
+            else
+            {
+                pictureBox4.Image = Properties.Resources._00;
+                pictureBox4.Image.Tag = "00";
+            }
+            timeClicked++;
+            labelTimeClicked.Text = "Time Clicked: " + timeClicked.ToString();
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            bool clicked;
+            if ((timeClicked % 2) == 0)
+            {
+                clicked = false;
+            }
+            else
+            {
+                clicked = true;
+            }
+            if (clicked == false)
+            {
+                pictureBox2.Image = Properties.Resources._01;
+                pictureBox2.Image.Tag = "01";
+            }
+            else
+            {
+                pictureBox2.Image = Properties.Resources._00;
+                pictureBox2.Image.Tag = "00";
+            }
+            timeClicked++;
+            labelTimeClicked.Text = "Time Clicked: " + timeClicked.ToString();
+        }
+    }
+}
